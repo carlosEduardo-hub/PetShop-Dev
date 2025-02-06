417f106ba6af982f569bad24706929198da72250
carlosEduardo-hub <carloseduardo010616@gmail.com>
2025-02-06 00:37:43 -0300

    full aplication

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { AosInit } from "./_components/aos-init";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PetShop - Dev",
+  description: "PetShop para animais de estimação",
+   icons: "/pet-shop.png",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <AosInit />
+      </body>
+    </html>
+  );
+}
